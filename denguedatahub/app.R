8fa8690f24ddc9c7ec298d195c8213c221f2ca61
thiyangt <thiyanga.talagala@monash.edu>
2023-04-14 20:52:47 +0530

    updated shiny app

--- denguedatahub/app.R
+++ denguedatahub/app.R
@@ -1,16 +1,55 @@
-library(DT)
-
+library(shiny)
 library(denguedatahub)
 
-ui <- basicPage(
-    h2("Dengue Data Hub"),
-    DT::dataTableOutput("mytable")
+ui <- fluidPage(
+  title = "Dengue Data Hub",
+  sidebarLayout(
+    sidebarPanel(
+      conditionalPanel(
+        'input.dataset === "level_of_risk"',
+        checkboxGroupInput("show_vars", "Columns in level_of_risk to show:",
+                           names(level_of_risk), selected = names(level_of_risk))
+      ),
+      conditionalPanel(
+        'input.dataset === "srilanka_weekly_data"',
+        checkboxGroupInput("show_vars", "Columns in srilanka_weekly_data to show:",
+                           names(srilanka_weekly_data), selected = names(srilanka_weekly_data))
+      ),
+      conditionalPanel(
+        'input.dataset === "world_annual"',
+        checkboxGroupInput("show_vars", "Columns in world_annual to show:",
+                           names(world_annual), selected = names(world_annual))
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = 'dataset',
+        tabPanel("level_of_risk", DT::dataTableOutput("mytable1")),
+        tabPanel("srilanka_weekly_data", DT::dataTableOutput("mytable2")),
+        tabPanel("world_annual", DT::dataTableOutput("mytable3"))
+      )
+    )
+  )
 )
 
 server <- function(input, output) {
-    output$mytable = DT::renderDataTable({
-        srilanka_weekly_data
-    })
+  
+  # choose columns to display
+  
+  output$mytable1 <- DT::renderDataTable({
+    DT::datatable(level_of_risk, options = list(orderClasses = TRUE))
+  })
+  
+  # sorted columns are colored now because CSS are attached to them
+  output$mytable2 <- DT::renderDataTable({
+    DT::datatable(srilanka_weekly_data, options = list(orderClasses = TRUE))
+  })
+  
+  # customize the length drop-down menu; display 5 rows per page by default
+  output$mytable3 <- DT::renderDataTable({
+    DT::datatable(world_annual, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+  })
+  
 }
 
 shinyApp(ui, server)
