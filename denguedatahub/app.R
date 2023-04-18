b42b2733e7adf9e24d715eec260ea4480d2a12ad
thiyangt <thiyanga.talagala@monash.edu>
2023-04-18 14:25:12 +0530

    updated app

--- denguedatahub/app.R
+++ denguedatahub/app.R
@@ -1,8 +1,15 @@
 library(shiny)
 library(denguedatahub)
+library(shinydashboard)
 
 ui <- fluidPage(
   title = "Dengue Data Hub",
+  dashboardHeader(
+    title= div(h1('Dengue Data Hub', style="margin: 0;"), h3('Creator: Thiyanga S. Talagala', style="margin: 0;"), h4('Web: https://denguedatahub.netlify.app/', style="margin: 0;"))
+  ),
+  
+  img(src='logo.png', align = "right"),
+  
   sidebarLayout(
     sidebarPanel(
       conditionalPanel(
@@ -21,7 +28,7 @@ ui <- fluidPage(
                            names(world_annual), selected = names(world_annual))
       ),
       conditionalPanel(
-        'input.dataset === "singapore_weekly_data"',
+       'input.dataset === "singapore_weekly_data"',
         checkboxGroupInput("show_vars", "Columns in singapore_weekly_data to show:",
                            names(singapore_weekly_data), selected = names(singapore_weekly_data))
       ),
@@ -29,7 +36,7 @@ ui <- fluidPage(
         'input.dataset === "china_anuual_data"',
         checkboxGroupInput("show_vars", "Columns in singapore_weekly_data to show:",
                            names(china_annual_data), selected = names(china_annual_data))
-      ),
+      )
     ),
     mainPanel(
       tabsetPanel(
@@ -37,7 +44,7 @@ ui <- fluidPage(
         tabPanel("level_of_risk", DT::dataTableOutput("mytable1")),
         tabPanel("srilanka_weekly_data", DT::dataTableOutput("mytable2")),
         tabPanel("world_annual", DT::dataTableOutput("mytable3")),
-        tabPanel("singapore_weekly_data", DT::dataTableOutput("mytable3")),
+        tabPanel("singapore_weekly_data", DT::dataTableOutput("mytable4")),
         tabPanel("china_annual_data", DT::dataTableOutput("mytable5"))
         
       )
@@ -50,27 +57,27 @@ server <- function(input, output) {
   # choose columns to display
   
   output$mytable1 <- DT::renderDataTable({
-    DT::datatable(level_of_risk, options = list(orderClasses = TRUE))
+    DT::datatable(level_of_risk, options = list(orderClasses = TRUE, iDisplayLength = 30))
   })
   
   # sorted columns are colored now because CSS are attached to them
   output$mytable2 <- DT::renderDataTable({
-    DT::datatable(srilanka_weekly_data, options = list(orderClasses = TRUE))
+    DT::datatable(srilanka_weekly_data, options = list(orderClasses = TRUE, iDisplayLength = 30))
   })
   
   # customize the length drop-down menu; display 5 rows per page by default
   output$mytable3 <- DT::renderDataTable({
-    DT::datatable(world_annual, options = list(orderClasses = TRUE))
+    DT::datatable(world_annual, options = list(orderClasses = TRUE, iDisplayLength = 30))
   })
   
-  # customize the length drop-down menu; display 5 rows per page by default
+ #  customize the length drop-down menu; display 5 rows per page by default
   output$mytable4 <- DT::renderDataTable({
-    DT::datatable(singapore_weekly_data, options = list(orderClasses = TRUE))
+    DT::datatable(singapore_weekly_data, options = list(orderClasses = TRUE, iDisplayLength = 30))
   })
   
   # customize the length drop-down menu; display 5 rows per page by default
   output$mytable5 <- DT::renderDataTable({
-    DT::datatable(china_annual_data, options = list(orderClasses = TRUE))
+    DT::datatable(china_annual_data, options = list(orderClasses = TRUE, iDisplayLength = 30))
   })
 }
 
