a7c3afe0edfa3d6488121fa846f216482dd0b047
thiyangt <thiyanga.talagala@monash.edu>
2023-04-23 07:24:15 +0530

    updated

--- denguedatahub/app.R
+++ denguedatahub/app.R
@@ -41,7 +41,7 @@ ui <- fluidPage(
     mainPanel(
       tabsetPanel(
         id = 'dataset',
-        tabPanel("level_of_risk", DT::dataTableOutput("mytable1")),
+        tabPanel("level_of_risk", DT::dataTableOutput("mytable1", )),
         tabPanel("srilanka_weekly_data", DT::dataTableOutput("mytable2")),
         tabPanel("world_annual", DT::dataTableOutput("mytable3")),
         tabPanel("singapore_weekly_data", DT::dataTableOutput("mytable4")),
@@ -57,27 +57,67 @@ server <- function(input, output) {
   # choose columns to display
   
   output$mytable1 <- DT::renderDataTable({
-    DT::datatable(level_of_risk, options = list(orderClasses = TRUE, iDisplayLength = 30))
+    DT::datatable(level_of_risk, extensions = "Buttons", options = list(orderClasses = TRUE, iDisplayLength = 30,
+                                                                        paging = TRUE,
+                                                                        scrollX=TRUE, 
+                                                                        searching = TRUE,
+                                                                        ordering = TRUE,
+                                                                        dom = 'Bfrtip',
+                                                                        buttons = c('copy', 'csv', 'excel', 'pdf'),
+                                                                    
+                                                                        lengthMenu=c(3,5,10)))
   })
   
   # sorted columns are colored now because CSS are attached to them
   output$mytable2 <- DT::renderDataTable({
-    DT::datatable(srilanka_weekly_data, options = list(orderClasses = TRUE, iDisplayLength = 30))
+    DT::datatable(srilanka_weekly_data, extensions = "Buttons", options = list(orderClasses = TRUE, iDisplayLength = 30,
+                                                                               paging = TRUE,
+                                                                               scrollX=TRUE, 
+                                                                               searching = TRUE,
+                                                                               ordering = TRUE,
+                                                                               dom = 'Bfrtip',
+                                                                               buttons = c('copy', 'csv', 'excel', 'pdf'),
+                                                                               
+                                                                               lengthMenu=c(3,5,10)))
   })
   
   # customize the length drop-down menu; display 5 rows per page by default
   output$mytable3 <- DT::renderDataTable({
-    DT::datatable(world_annual, options = list(orderClasses = TRUE, iDisplayLength = 30))
+    DT::datatable(world_annual, extensions = "Buttons", options = list(orderClasses = TRUE, iDisplayLength = 30,
+                                                                       paging = TRUE,
+                                                                       scrollX=TRUE, 
+                                                                       searching = TRUE,
+                                                                       ordering = TRUE,
+                                                                       dom = 'Bfrtip',
+                                                                       buttons = c('copy', 'csv', 'excel', 'pdf'),
+                                                                       
+                                                                       lengthMenu=c(3,5,10)))
   })
   
  #  customize the length drop-down menu; display 5 rows per page by default
   output$mytable4 <- DT::renderDataTable({
-    DT::datatable(singapore_weekly_data, options = list(orderClasses = TRUE, iDisplayLength = 30))
+    DT::datatable(singapore_weekly_data, extensions = "Buttons", options = list(orderClasses = TRUE, iDisplayLength = 30,
+                                                                                paging = TRUE,
+                                                                                scrollX=TRUE, 
+                                                                                searching = TRUE,
+                                                                                ordering = TRUE,
+                                                                                dom = 'Bfrtip',
+                                                                                buttons = c('copy', 'csv', 'excel', 'pdf'),
+                                                                                
+                                                                                lengthMenu=c(3,5,10)))
   })
   
   # customize the length drop-down menu; display 5 rows per page by default
   output$mytable5 <- DT::renderDataTable({
-    DT::datatable(china_annual_data, options = list(orderClasses = TRUE, iDisplayLength = 30))
+    DT::datatable(china_annual_data, extensions = "Buttons", options = list(orderClasses = TRUE, iDisplayLength = 30,
+                                                                            paging = TRUE,
+                                                                            scrollX=TRUE, 
+                                                                            searching = TRUE,
+                                                                            ordering = TRUE,
+                                                                            dom = 'Bfrtip',
+                                                                            buttons = c('copy', 'csv', 'excel', 'pdf'),
+                                                                            
+                                                                            lengthMenu=c(3,5,10)))
   })
 }
 
